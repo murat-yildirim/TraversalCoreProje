8f8c2bdb8d2ff33c167c9c8cfb8c86d2626618b8
Murat Yıldırım <30393559+murat-yildirim@users.noreply.github.com>
2025-02-17 23:58:50 +0300

    76.Ders sonu Unit Of Work projeye eklenmesi

---
+++ BusinessLayer/Abstract/AbstractUow/IGenericUowService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract.AbstractUow
+{
+    public interface IGenericUowService<T>
+    {
+        void TInsert(T t); //T türünde t parametresi almasını istiyoruz
+        void TUpgrade(T t);
+
+        void TMultiUpdate(List<T> t);
+
+        T TGetByID(int id);
+    }
+}
---
+++ DataAccessLayer/Abstract/IGenericUowDal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IGenericUowDal <T> where T : class
+    {
+        void Insert(T t); //T türünde t parametresi almasını istiyoruz
+        void Upgrade(T t);
+
+        void MultiUpdate(List<T> t);
+
+        T GetByID(int id);
+       
+    }
+}
