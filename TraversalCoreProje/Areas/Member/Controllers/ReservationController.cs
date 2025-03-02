d52fee0ec67c1bec9651dac8cf20d801d52823c5
Murat Yıldırım <30393559+murat-yildirim@users.noreply.github.com>
2025-03-02 15:53:30 +0300

    94.Ders sonu kullanıcı dashboard sayfasında komple değişiklik

--- TraversalCoreProje/Areas/Member/Controllers/ReservationController.cs
+++ TraversalCoreProje/Areas/Member/Controllers/ReservationController.cs
@@ -64,5 +64,10 @@ namespace TraversalCoreProje.Areas.Member.Controllers
             reservationManager.TAdd(p);
             return RedirectToAction("MyCurrentReservation");
         }
+
+        public IActionResult Deneme()
+        {
+            return View();
+        }
     }
 }
