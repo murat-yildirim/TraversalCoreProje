bebb6bb9d5f2e49c9adbbca2120e9f76c9242786
Murat Yıldırım <30393559+murat-yildirim@users.noreply.github.com>
2025-03-03 21:50:21 +0300

    98.Ders sonu şifremi unuttum reset password işlemleri tamamlandı

--- TraversalCoreProje/Startup.cs
+++ TraversalCoreProje/Startup.cs
@@ -6,6 +6,7 @@ using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.Configuration;
@@ -47,7 +48,7 @@ namespace TraversalCoreProje
 
 			services.AddDbContext<Context>();
 			services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<Context>()
-				.AddErrorDescriber<CustomIdentityValidator>().AddEntityFrameworkStores<Context>();
+				.AddErrorDescriber<CustomIdentityValidator>().AddTokenProvider<DataProtectorTokenProvider<AppUser>>(TokenOptions.DefaultProvider).AddEntityFrameworkStores<Context>();
 
 			services.AddHttpClient();
 
